99f2c1789eab78fd44c5bff9021f8db309a9f3d4
Pranjal Upadhyay <pranjalupadhyay13@gmail.com>
2025-09-09 14:29:07 +0530

    added cascade delete middleware in project model

--- backend/model/Project.js
+++ backend/model/Project.js
@@ -24,6 +24,26 @@ const projectSchema = new mongoose.Schema({
   }
 })
 
+projectSchema.pre("findOneAndDelete", async function(next){
+  const filter = this.getFilter(); //extracting the filter with which findOneAndDelete on Project model was called
+  const projectId = filter._id //pre("findOneAndDelete") is a query middleware not document middleware, so we cant just access the id throught this._id, we have to extract it from the query
+
+  if (projectId){
+    //cascading deletes in student
+    await mongoose.model("Student").updateMany(
+      {projectIds : projectId}, //filter out all the students who have this projectId in their projectIds arrray
+      {$pull : {projectIds : projectId}} // do this operation where you update the projectIds array and remove instance of the projectId from each Student model it was found in
+    )
+
+    //cascading deletes in faculty
+    await mongoose.model("Faculty").updateMany(
+      {projectIds: projectId},
+      {$pull: {projectIds: projectId}}
+    )
+  }
+  next()
+})
+
 const Project = mongoose.model("Project", projectSchema)
 
 module.exports = Project
